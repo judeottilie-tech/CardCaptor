b87fd06318a024ad0be620716793046cad612528
Jude Andersen <jude.ottilie@gmail.com>
2026-07-19 18:08:13 -0500

    binder page card slot done

--- Controllers/BinderPageCardSlotController.cs
+++ Controllers/BinderPageCardSlotController.cs
@@ -12,6 +12,56 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CardCaptor.Controllers;
 
-//put /api/bindercardslot/{id}/card //allows add replace of card in slot
+[ApiController]
+[Route("api/[controller]")]
+public class BinderPageCardSlotController : ControllerBase
+{
+    private CardCaptorDbContext _dbContext;
 
-//delete /api/bindercardslot/{id}/card // clears the slot
+    public BinderPageCardSlotController(CardCaptorDbContext context)
+    {
+        _dbContext = context;
+    }
+
+    [HttpPut("{id}/card")]
+    [Authorize]
+    public IActionResult Update(int id, AttachCardDTO dto)
+    {
+        var identityUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var profile = _dbContext.UserProfiles.SingleOrDefault(up => up.IdentityUserId == identityUserId);
+        if (profile == null) return NotFound();
+
+        var slot = _dbContext.BinderPageCardSlots
+            .Include(bpcs => bpcs.BinderPage)
+            .SingleOrDefault(bpcs => bpcs.Id == id);
+
+        if (slot == null) return NotFound();
+        if (slot.BinderPage.UserProfileId != profile.Id) return NotFound();
+
+        slot.CardId = dto.CardId;
+        _dbContext.SaveChanges();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}/card")]
+    [Authorize]
+    public IActionResult Delete(int id)
+    {
+        var identityUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var profile = _dbContext.UserProfiles.SingleOrDefault(up => up.IdentityUserId == identityUserId);
+        if (profile == null) return NotFound();
+
+        var slot = _dbContext.BinderPageCardSlots
+            .Include(bpcs => bpcs.BinderPage)
+            .SingleOrDefault(bpcs => bpcs.Id == id);
+
+        if (slot == null) return NotFound();
+        if (slot.BinderPage.UserProfileId != profile.Id) return NotFound();
+
+        slot.CardId = null;
+        _dbContext.SaveChanges();
+
+        return NoContent();
+    }
+}
