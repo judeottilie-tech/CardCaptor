213a68239fdd28f1dc4717b8354a3fa1935348b4
Jude Andersen <jude.ottilie@gmail.com>
2026-07-27 14:59:30 -0500

    styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Quicksand", "sans-serif"],
+        body: ["Lexend", "sans-serif"],
+      },
+      colors: {
+        brand: {
+          cream: "#F8EADC",
+          blush: "#FAD0D5",
+          rose: "#F59BAD",
+          periwinkle: "#778BBE",
+          sky: "#9EDEF9",
+          ink: "#2B2438",
+        },
+      },
+    },
   },
   plugins: [],
 }
