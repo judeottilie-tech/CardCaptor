a807e66b36a2ffca52aa7d5bdad61fabd8829027
Jude Andersen <jude.ottilie@gmail.com>
2026-07-27 11:29:51 -0500

    added controllers to card

--- Controllers/CardController.cs
+++ Controllers/CardController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using CardCaptor.Data;
 namespace CardCaptor.Controllers;
 
+using Microsoft.EntityFrameworkCore;
+
+
 [ApiController]
 [Route("api/[controller]")]
 public class CardController : ControllerBase
@@ -16,8 +19,31 @@ public class CardController : ControllerBase
 
     [HttpGet]
     [Authorize]
-    public IActionResult Get()
+    public IActionResult Get(string? search, string? category, string? rarity, string? era)
     {
-        return Ok(_dbContext.Cards.ToList());
+        var query = _dbContext.Cards.AsQueryable();
+
+        if (!string.IsNullOrWhiteSpace(search))
+        {
+            query = query.Where(c => EF.Functions.ILike(c.Name, $"%{search}%"));
+        }
+
+        if (!string.IsNullOrWhiteSpace(category))
+        {
+            query = query.Where(c => c.Category == category);
+        }
+
+        if (!string.IsNullOrWhiteSpace(rarity))
+        {
+            query = query.Where(c => c.Rarity == rarity);
+        }
+
+        if (!string.IsNullOrWhiteSpace(era))
+        {
+            query = query.Where(c => c.Era == era);
+        }
+
+        return Ok(query.ToList());
     }
 };
+
--- Models/Card.cs
+++ Models/Card.cs
@@ -5,5 +5,11 @@ public class Card
     public int Id { get; set; }
     public string Name { get; set; }
     public string ImageUrl { get; set; }
+    public string Rarity { get; set; }
+    public string Types { get; set; }
+    public string Category { get; set; }
+    public string SetName { get; set; }
+    public string Era { get; set; }
+    public string SourceId { get; set; }
 
 };
